2249a07548ee52cac768b3f0dc50b774258617cc
mushan520 <55680267+mushan520@users.noreply.github.com>
2019-09-25 20:02:01 +0800

    Update wc.java

--- wcproject/src/com/zwb/wc/wc.java
+++ wcproject/src/com/zwb/wc/wc.java
@@ -39,9 +39,7 @@ public class wc {
                 return;
             } else {
                 for (File file2 : files) {
-                    if (file2.isDirectory()) {                    
-                        foundFilePath(file2.getAbsolutePath(), fileName);
-                    } else if (file2.getName().contains(fileName)) {
+                     if (file2.getName().contains(fileName)) {
                     	System.out.println("文件:" + file2.getAbsolutePath());
                     	count.command(file2.getAbsolutePath(), "-all");
 					}
@@ -52,6 +50,7 @@ public class wc {
         }
     }
 }
+
 
 
 class count{
